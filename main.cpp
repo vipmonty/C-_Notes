48bb3213f7130553b35aa95100e935a45243529b
Viphakone Monty <10835390@my.uvu.edu>
2024-01-10 18:08:03 -0700

    adding files

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    int my_age = 36;
+    double weight = 122;
+    std::string message = "My name is Viphakone Monty and I am ";
+    std::string message2 = ", I also weigh ";
+    std::cout << message << my_age << message2 << weight << std::endl;
+
+    return 0;
+};
