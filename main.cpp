af0d9241ab7c7346fb6fa369d1e18f862f8fd748
Vip Monty <10835390@uvu.edu>
2024-01-30 10:41:36 -0700

    updated on 1/30/24

--- main.cpp
+++ main.cpp
@@ -1,13 +1,33 @@
 #include <iostream>
 #include <string>
+#include <math.h>
+
+using namespace std;
+
+int addTwoNums (int numOne, int numTwo) {
+    return numOne + numTwo;
+ };
 
 int main()
 {
-    int my_age = 36;
-    double weight = 122;
-    std::string message = "My name is Viphakone Monty and I am ";
-    std::string message2 = ", I also weigh ";
-    std::cout << message << my_age << message2 << weight << std::endl;
+    // int my_age = 36;
+    // double weight = 122;
+    // std::string message = "My name is Viphakone Monty and I am ";
+    // std::string message2 = ", I also weigh ";
+    // std::cout << message << my_age << message2 << weight << std::endl;
 
+    // return 0;
+
+    int numOne = 1;
+    int numTwo = 3;
+
+    cout << 'Enter two numbers to add' << endl;
+    cout << 'Number 1:' << endl;
+    cin >> numOne;
+
+    cout << 'Number 2: ' << endl;
+    cin >> numTwo;
+
+    cout << numOne << ' + ' << numTwo << ' = ' << addTwoNums(numOne, numTwo) << endl;
     return 0;
 };
